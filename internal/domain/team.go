344e1b2a468b5b72938904131ba25bd540424d74
mivihan <mvgliatser@edu.hse.ru>
2025-11-22 20:27:07 +0300

    fixed team valid + domain tests

--- internal/domain/team.go
+++ internal/domain/team.go
@@ -1,39 +1,19 @@
 package domain
 
 import (
-	"fmt"
-	"strings"
-	"time"
+    "fmt"
+    "strings"
+    "time"
 )
 
 type Team struct {
-	TeamName  string
-	Members   []*User
-	CreatedAt time.Time
-	UpdatedAt time.Time
+    TeamName  string
+    CreatedAt time.Time
 }
 
 func (t *Team) Validate() error {
-	if strings.TrimSpace(t.TeamName) == "" {
-		return fmt.Errorf("team_name cannot be empty")
-	}
-	if t.Members == nil {
-		return fmt.Errorf("members cannot be nil")
-	}
-	return nil
-}
-
-func (t *Team) GetActiveMembersExcluding(excludeUserIDs ...string) []*User {
-	excludeMap := make(map[string]bool)
-	for _, id := range excludeUserIDs {
-		excludeMap[id] = true
-	}
-
-	activeMembers := make([]*User, 0)
-	for _, member := range t.Members {
-		if member.IsActive && !excludeMap[member.UserID] {
-			activeMembers = append(activeMembers, member)
-		}
-	}
-	return activeMembers
+    if strings.TrimSpace(t.TeamName) == "" {
+        return fmt.Errorf("team_name cannot be empty")
+    }
+    return nil
 }
